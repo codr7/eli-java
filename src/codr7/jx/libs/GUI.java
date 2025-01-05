3ed3ded87f3c57b8d6836c521633ab9cee223de7
Andreas Nilsson <codr7@protonmail.com>
2025-01-05 13:19:46 +0100

    Add tab view.

--- src/codr7/jx/libs/GUI.java
+++ src/codr7/jx/libs/GUI.java
@@ -3,7 +3,9 @@ package codr7.jx.libs;
 import codr7.jx.Arg;
 import codr7.jx.Lib;
 import codr7.jx.Value;
+import codr7.jx.libs.gui.types.ContainerType;
 import codr7.jx.libs.gui.types.FrameType;
+import codr7.jx.libs.gui.types.TabViewType;
 import codr7.jx.libs.gui.types.WidgetType;
 
 import javax.swing.*;
@@ -11,16 +13,49 @@ import java.awt.*;
 
 public class GUI extends Lib {
     public static final WidgetType widgetType = new WidgetType("Widget");
+    public static final ContainerType containerType = new ContainerType("Container", widgetType);
     public static final FrameType frameType = new FrameType("Frame", widgetType);
+    public static final TabViewType tabViewType = new TabViewType("TabView", containerType);
 
     public GUI() {
         super("gui");
 
+        bind(containerType);
         bind(frameType);
+        bind(tabViewType);
         bind(widgetType);
 
-        bindMethod("new-frame",
-                new Arg[]{new Arg("title", Core.stringType), new Arg("size", Core.pairType)}, null,
+        bindMethod("add", new Arg[]{new Arg("parent", containerType), new Arg("child", widgetType)}, null,
+                (vm, args, rResult, location) -> {
+                    final var p = args[0].cast(containerType);
+                    final var w = args[1].cast(widgetType);
+                    p.add(w);
+                });
+
+        bindMethod("add-tab",
+                new Arg[]{new Arg("parent", tabViewType),
+                        new Arg("title", Core.stringType),
+                new Arg("child", widgetType)}, null,
+                (vm, args, rResult, location) -> {
+                    final var title = args[1].cast(Core.stringType);
+                    final var child = args[2].cast(widgetType);
+                    args[0].cast(tabViewType).add(title, child);
+                });
+
+        bindMethod("box-layout", new Arg[]{new Arg("target", containerType)}, null,
+                (vm, args, rResult, location) -> {
+                    final var t = args[0].cast(containerType);
+                    t.setLayout(new BoxLayout(t, BoxLayout.PAGE_AXIS));
+                });
+
+        bindMethod("content", new Arg[]{new Arg("frame", frameType)}, containerType,
+                (vm, args, rResult, location) -> {
+                    final var f = args[0].cast(frameType);
+                    vm.registers.set(rResult, new Value<>(containerType, f.getContentPane()));
+                });
+
+        bindMethod("frame",
+                new Arg[]{new Arg("title", Core.stringType), new Arg("size", Core.pairType)}, frameType,
                 (vm, args, rResult, location) -> {
                     final var title = args[0].cast(Core.stringType);
                     final var f = new JFrame(title);
@@ -35,6 +70,19 @@ public class GUI extends Lib {
         bindMethod("pack", new Arg[]{new Arg("frames*")}, null,
                 (vm, args, rResult, location) -> {
                     for (final var a: args) { a.cast(frameType).pack(); }
+                });
+
+        bindMethod("panel",
+                new Arg[]{}, null,
+                (vm, args, rResult, location) -> {
+                    final var p = new JPanel();
+                    p.setLayout(new BorderLayout());
+                    vm.registers.set(rResult, new Value<>(containerType, p));
+                });
+
+        bindMethod("tabs", new Arg[]{}, containerType,
+                (vm, args, rResult, location) -> {
+                    vm.registers.set(rResult, new Value<>(widgetType, new JTabbedPane()));
                 });
 
         bindMethod("show", new Arg[]{new Arg("widgets*")}, null,
---
+++ src/codr7/jx/libs/gui/types/ContainerType.java
@@ -0,0 +1,10 @@
+package codr7.jx.libs.gui.types;
+
+import codr7.jx.BaseType;
+import codr7.jx.IType;
+
+import java.awt.*;
+
+public class ContainerType extends BaseType<Container> {
+    public ContainerType(final String id, final IType...parentTypes) { super(id, parentTypes); }
+}
