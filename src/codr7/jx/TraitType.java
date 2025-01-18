8f7934861f8790cc9568d178838226ae4c39a900
Andreas Nilsson <codr7@protonmail.com>
2025-01-18 04:14:56 +0100

    Move type.

--- src/codr7/jx/TraitType.java
+++ src/codr7/jx/TraitType.java
@@ -1,9 +1,4 @@
-package codr7.jx.libs.core.types;
-
-import codr7.jx.BaseType;
-import codr7.jx.IType;
-import codr7.jx.IValue;
-import codr7.jx.VM;
+package codr7.jx;
 
 public class TraitType extends BaseType<Void> {
     public TraitType(final String id, final IType...parentTypes) { super(id, parentTypes); }
