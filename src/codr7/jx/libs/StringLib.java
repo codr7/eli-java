10107fde506e5758b1ac2a15dc74a7cec4986f42
Andreas Nilsson <codr7@protonmail.com>
2025-01-21 16:49:29 +0100

    Added string/strip.

--- src/codr7/jx/libs/StringLib.java
+++ src/codr7/jx/libs/StringLib.java
@@ -8,12 +8,20 @@ public class StringLib extends Lib {
     public StringLib() {
         super("string");
 
-        bindMethod("find", new Arg[]{new Arg("in", CoreLib.stringType), new Arg("it", CoreLib.stringType)}, null,
+        bindMethod("find", new Arg[]{new Arg("in", CoreLib.stringType), new Arg("it", CoreLib.stringType)}, CoreLib.intType,
                 (vm, args, rResult, loc) -> {
                         final var in = args[0].cast(CoreLib.stringType);
                         final var it = args[0].cast(CoreLib.stringType);
                         final var i = in.indexOf(it);
                         vm.registers.set(rResult, (i == -1) ? CoreLib.NIL : new Value<>(CoreLib.intType, (long) i));
+                });
+
+        bindMethod("strip", new Arg[]{new Arg("in", CoreLib.stringType), new Arg("it", CoreLib.anyType)}, CoreLib.stringType,
+                (vm, args, rResult, loc) -> {
+                    final var set = args[1].cast(CoreLib.charType);
+                    final var in = args[0].cast(CoreLib.stringType);
+                    final var out = in.replaceAll("[" + set + "]", "");
+                    vm.registers.set(rResult, new Value<>(CoreLib.stringType, out));
                 });
     }
 }
