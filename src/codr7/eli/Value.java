9d2caa3d4b55ad64918ace22d9b742d06cf0c436
Andreas Nilsson <codr7@protonmail.com>
2025-03-08 17:25:08 +0100

    Move expand to value.

--- src/codr7/eli/Value.java
+++ src/codr7/eli/Value.java
@@ -1,6 +1,26 @@
 package codr7.eli;
 
+import codr7.eli.libs.CoreLib;
+
+import java.util.List;
+
 public record Value<T>(IDataType<T> type, T data) implements IValue {
+    public static void expand(final VM vm,
+                              final IValue value,
+                              final List<IValue> out,
+                              final Loc loc) {
+        if (value.type() == CoreLib.splatType) {
+            final var rValue = vm.alloc(1);
+            final var it = value.cast(CoreLib.splatType);
+
+            while (it.next(vm, rValue, loc)) {
+                expand(vm, vm.registers.get(rValue), out, loc);
+            }
+        } else {
+            out.add(value);
+        }
+    }
+
     public <U> U cast(IDataType<U> type) { return (U)data; }
 
     public IValue dup(final VM vm) { return type.dup(vm, this); }
--- src/codr7/eli/libs/core/traits/CallTrait.java
+++ src/codr7/eli/libs/core/traits/CallTrait.java
@@ -1,32 +1,11 @@
 package codr7.eli.libs.core.traits;
 
-import codr7.eli.IForm;
-import codr7.eli.IValue;
-import codr7.eli.Loc;
-import codr7.eli.VM;
-import codr7.eli.forms.SplatForm;
-import codr7.eli.libs.CoreLib;
+import codr7.eli.*;
 import codr7.eli.ops.CallValue;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public interface CallTrait {
-    static void expandArg(final VM vm,
-                          final IValue value,
-                          final List<IValue> out,
-                          final Loc loc) {
-        if (value.type() == CoreLib.splatType) {
-            final var rValue = vm.alloc(1);
-            final var it = value.cast(CoreLib.splatType);
-
-            while (it.next(vm, rValue, loc)) {
-                expandArg(vm, vm.registers.get(rValue), out, loc);
-            }
-        } else {
-            out.add(value);
-        }
-    }
 
     default void call(final VM vm,
                       final IValue target,
@@ -39,7 +18,7 @@ public interface CallTrait {
 
         for (var i = 0; i < arity; i++) {
             final var v = vm.registers.get(rArgs+i);
-            CallTrait.expandArg(vm, v, args, loc);
+            Value.expand(vm, v, args, loc);
         }
 
         call(vm, target, args.toArray(IValue[]::new), rResult, eval, loc);
