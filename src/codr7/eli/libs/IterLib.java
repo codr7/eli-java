525d469a346df6858cd971430ab4960937d2eadd
Andreas Nilsson <codr7@protonmail.com>
2025-03-15 00:52:37 +0100

    Renames.

--- src/codr7/eli/libs/IterLib.java
+++ src/codr7/eli/libs/IterLib.java
@@ -13,7 +13,7 @@ public final class IterLib extends Lib {
     public IterLib() {
         super("iter", null);
 
-        bindMethod("combinations",
+        bindMethod("comb",
                 new Arg[]{new Arg("in", CoreLib.Iterable)},
                 (vm, args, rResult, loc) -> {
                     final var in = args[0];
@@ -27,7 +27,8 @@ public final class IterLib extends Lib {
                         }
 
                         final var out = Utils.combine(ol.toArray(IValue[]::new));
-                        final Stream<IValue> s = out.map(l -> new Value<>(CoreLib.List, new ArrayList<>(l)));
+                        final Stream<IValue> s = out.map(l ->
+                                new Value<>(CoreLib.List, new ArrayList<>(l)));
                         vm.registers.set(rResult, new Value<>(CoreLib.Iter, new StreamItems(s)));
                     }
                 });
@@ -58,6 +59,25 @@ public final class IterLib extends Lib {
                     vm.registers.set(rResult, new Value<>(CoreLib.List, out));
                 });
 
+        bindMethod("fold",
+                new Arg[]{new Arg("callback", CoreLib.Callable),
+                        new Arg("in", CoreLib.Iterable),
+                        new Arg("seed", CoreLib.Any)},
+                (vm, args, rResult, loc) -> {
+                    final var c = args[0];
+                    final var ct = c.type().cast(CoreLib.Callable, loc);
+                    final var in = args[1];
+                    final var it = in.type().cast(CoreLib.Iterable, loc);
+                    final var rArgs = vm.alloc(2);
+                    vm.registers.set(rArgs, args[2]);
+
+                    for (final var i = it.iter(vm, in); i.next(vm, rArgs + 1, loc); ) {
+                        ct.call(vm, c, rArgs, 2, rArgs, true, loc);
+                    }
+
+                    vm.registers.set(rResult, vm.registers.get(rArgs));
+                });
+
         bindMethod("map",
                 new Arg[]{new Arg("callback", CoreLib.Callable),
                         new Arg("in*", CoreLib.Iterable)},
@@ -78,25 +98,6 @@ public final class IterLib extends Lib {
                     if (!args[0].cast(CoreLib.Iter).next(vm, rResult, loc)) {
                         vm.registers.set(rResult, CoreLib.NIL);
                     }
-                });
-
-        bindMethod("reduce",
-                new Arg[]{new Arg("callback", CoreLib.Callable),
-                        new Arg("in", CoreLib.Iterable),
-                        new Arg("seed", CoreLib.Any)},
-                (vm, args, rResult, loc) -> {
-                    final var c = args[0];
-                    final var ct = c.type().cast(CoreLib.Callable, loc);
-                    final var in = args[1];
-                    final var it = in.type().cast(CoreLib.Iterable, loc);
-                    final var rArgs = vm.alloc(2);
-                    vm.registers.set(rArgs, args[2]);
-
-                    for (final var i = it.iter(vm, in); i.next(vm, rArgs + 1, loc); ) {
-                        ct.call(vm, c, rArgs, 2, rArgs, true, loc);
-                    }
-
-                    vm.registers.set(rResult, vm.registers.get(rArgs));
                 });
 
         bindMethod("unzip", new Arg[]{new Arg("in", CoreLib.Iterable)},
@@ -127,7 +128,7 @@ public final class IterLib extends Lib {
 
         vm.eval("""
                 (^sum [in]
-                  (reduce + in 0))
+                  (fold + in 0))
                 """, loc);
     }
 }
